7629aa52ee2e722451cbdd470acb67b199e9deac
Michael Eaton <meaton@xby2.com>
2019-06-22 11:18:14 -0700

    Missing semicolon

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,4 +1,4 @@
 export const CLIENT_ID = 'client_id';
 export const CLIENT_SECRET = 'client_secret';
 export const REDIRECT_URI = 'redirect_uri';
-export const SCOPES = 'playlist-read-private playlist-modify-private user-modify-playback-state user-read-currently-playing'
+export const SCOPES = 'playlist-read-private playlist-modify-private user-modify-playback-state user-read-currently-playing';
